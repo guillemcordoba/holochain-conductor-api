82f621caca206755d81adf7eddf7af7e2a70ea90
Guillem Cordoba <guillem.cordoba@gmail.com>
2020-10-22 18:21:27 +0200

    Fix bug

--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -59,7 +59,7 @@ export class WsClient {
           let data = encodedMsg.data;
 
           // If data is not a buffer, it will be a blob
-          if (!Buffer || !Buffer.isBuffer(data)) {
+          if (typeof Buffer === undefined || !Buffer.isBuffer(data)) {
             data = await data.arrayBuffer();
           }
 
